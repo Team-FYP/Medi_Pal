d2e0a008849da5b017a4e1d19cf38a2616da16f8
Lakshan Gamage <lakshangamage.13@cse.mrt.ac.lk>
2017-08-11 10:45:49 +0530

    added timeline

--- app/src/main/java/lk/ac/mrt/cse/medipal/adaptor/PrescriptionRecyclerAdaptor.java
+++ app/src/main/java/lk/ac/mrt/cse/medipal/adaptor/PrescriptionRecyclerAdaptor.java
@@ -1,63 +1,71 @@
 package lk.ac.mrt.cse.medipal.adaptor;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.vipulasri.timelineview.TimelineView;
+
 import java.util.ArrayList;
 
 import lk.ac.mrt.cse.medipal.R;
 import lk.ac.mrt.cse.medipal.model.Prescription;
+import lk.ac.mrt.cse.medipal.util.VectorDrawableUtil;
 
 /**
  * Created by chand on 2017-06-13.
  */
 
-public class PrescriptionRecyclerAdaptor extends RecyclerView.Adapter<PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder> implements View.OnClickListener{
-    ArrayList<Prescription> predictionList = null;
+public class PrescriptionRecyclerAdaptor extends RecyclerView.Adapter<PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder>{
+    ArrayList<Prescription> prescriptionList = null;
     Context context = null;
-    Activity activity;
-    public PrescriptionRecyclerAdaptor(ArrayList<Prescription> predictionList, Context context) {
+    public PrescriptionRecyclerAdaptor(ArrayList<Prescription> prescriptionList, Context context) {
         this.context = context;
-        this.predictionList = predictionList;
-        this.activity = activity;
+        this.prescriptionList = prescriptionList;
     }
 
     @Override
     public PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view;
         view = LayoutInflater.from(parent.getContext()).inflate(R.layout.prescription_row_layout,parent,false);
-        PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder recyclerViewHolder = new PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder(view, context, predictionList);
+        PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder recyclerViewHolder = new PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder(view, context, viewType);
         return recyclerViewHolder;
     }
     @Override
     public void onBindViewHolder(PrescriptionRecyclerAdaptor.PredictionRecyclerViewHolder holder, int position) {
-        Prescription prescription = predictionList.get(position);
+        Prescription prescription = prescriptionList.get(position);
     }
+
     @Override
     public int getItemCount() {
-        return predictionList.size();
+        return (prescriptionList!=null? prescriptionList.size():0);
     }
-    @Override
-    public void onClick(View view) {
 
+
+    @Override
+    public int getItemViewType(int position) {
+        return TimelineView.getTimeLineViewType(position,getItemCount());
     }
 
     public class PredictionRecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        ArrayList<Prescription> patientList = null;
+        ArrayList<Prescription> prescriptionList = null;
         Context context = null;
-        public PredictionRecyclerViewHolder(View itemView, Context context, ArrayList<Prescription> patientList) {
+        TimelineView timeLineView;
+
+        public PredictionRecyclerViewHolder(View itemView, Context context, int viewType) {
             super(itemView);
             this.context =context;
-            this.patientList = patientList;
+            timeLineView = (TimelineView) itemView.findViewById(R.id.time_marker);
+            timeLineView.setMarker(VectorDrawableUtil.getDrawable(context, R.drawable.icon_time_line_marker, R.color.colorPrimary));
+            this.prescriptionList = prescriptionList;
+            timeLineView.initLine(viewType);
         }
-
         @Override
         public void onClick(View view) {
         }
+
     }
 }
 
