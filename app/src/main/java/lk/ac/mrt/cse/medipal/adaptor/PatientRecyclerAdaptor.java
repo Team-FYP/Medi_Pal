a1d1f6222d8eeb916f92c349c26c42d3792549bc
Lakshan Gamage <lakshangamage.13@cse.mrt.ac.lk>
2017-11-10 12:04:15 +0530

    added patient row pop up

--- app/src/main/java/lk/ac/mrt/cse/medipal/adaptor/PatientRecyclerAdaptor.java
+++ app/src/main/java/lk/ac/mrt/cse/medipal/adaptor/PatientRecyclerAdaptor.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
@@ -76,13 +80,15 @@ public class PatientRecyclerAdaptor extends RecyclerView.Adapter<PatientRecycler
 
     }
 
-    public class PatientRecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public class PatientRecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, PopupMenu.OnMenuItemClickListener{
         TextView txt_name;
         TextView txt_mobile;
         TextView txt_nic;
+        ImageView btn_pup_up;
         SimpleDraweeView contact_icon;
         ArrayList<Patient> patientList = null;
         Context context = null;
+        PopupMenu popupMenu;
         public PatientRecyclerViewHolder(View itemView, Context context, ArrayList<Patient> patientList) {
             super(itemView);
             this.context =context;
@@ -90,15 +96,36 @@ public class PatientRecyclerAdaptor extends RecyclerView.Adapter<PatientRecycler
             txt_name = (TextView) itemView.findViewById(R.id.txt_name);
             txt_mobile = (TextView) itemView.findViewById(R.id.txt_mobile);
             txt_nic = (TextView) itemView.findViewById(R.id.txt_nic);
+            btn_pup_up = (ImageView) itemView.findViewById(R.id.btn_pop_up);
             contact_icon = (SimpleDraweeView) itemView.findViewById(R.id.icon_patient);
             contact_icon.setOnClickListener(this);
+            btn_pup_up.setOnClickListener(this);
+
+            popupMenu = new PopupMenu(context, btn_pup_up, Gravity.NO_GRAVITY, R.attr.actionOverflowMenuStyle, 0);
+            popupMenu.getMenuInflater().inflate(R.menu.patient_row_pop_up_menu, popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(this);
+
         }
 
         @Override
         public void onClick(View view) {
-            Intent intent = new Intent(context, PatientInfoActivity.class);
-            intent.putExtra("patient_id", getAdapterPosition());
-            context.startActivity(intent);
+            if (view.getId() == contact_icon.getId()){
+                Intent intent = new Intent(context, PatientInfoActivity.class);
+                intent.putExtra("patient_id", getAdapterPosition());
+                context.startActivity(intent);
+            } else if(view.getId() == btn_pup_up.getId()) {
+                popupMenu.show();
+            }
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            if (item.getItemId() ==  R.id.view) {
+                Intent intent = new Intent(context, PatientInfoActivity.class);
+                intent.putExtra("patient_id", getAdapterPosition());
+                context.startActivity(intent);
+            }
+            return true;
         }
     }
 }
